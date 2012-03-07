017de960a2bd2222913e1a7914869333ba693ad5
George Ornbo <george@shapeshed.com>
2012-03-07 12:13:12 +0000

    0.0.1 release

---
+++ README.md
@@ -0,0 +1,55 @@
+# Tokenator
+
+[![Build Status](https://secure.travis-ci.org/shapeshed/tokenator.png)](http://travis-ci.org/shapeshed/tokenator)
+
+Tokenator is a lightweight piece of [Connect][1] Middleware to provide access control based on a token. It is design to be used with APIs.
+
+## Installation
+
+via [npm][2]
+
+    npm install tokenator
+
+## Usage
+
+Tokenator expects the API token to be sent by the client in a header named 'api-token'. 
+
+On the server tokenator expects an array of valid tokens.
+
+If a client tries to connect without a token or an invalid token a 401 response will be returned. 
+
+For [connect][1]
+
+    var connect = require('connect')
+      , http = require('http')
+      , tokenator = require('tokenator');
+
+    function accessGranted(req, res){
+      res.end('Tokenator says yes!');
+    }
+    var app = connect()
+      .use(tokenator(['5917f138c80b512d14a4ee2fe05a17dc', '7b1a47ab847f7534b507c6ae4a763118']))
+      .use(accessGranted)
+     
+    http.Server(app).listen(3000);
+
+For [express][3]
+
+    var express = require('express')
+      , tokenator = require('tokenator');
+
+    var app = module.exports = express.createServer();
+
+    app.configure(function(){
+      app.use(tokenator(['5917f138c80b512d14a4ee2fe05a17dc', '7b1a47ab847f7534b507c6ae4a763118']))
+    });
+
+    app.get('/', function(req, res){
+      res.send('Tokenator says yes!');
+    });
+
+    app.listen(3000);
+
+[1]: https://github.com/senchalabs/connect/
+[2]: http://npmjs.org/
+[3]: http://expressjs.com/
---
+++ test/tokenator.test.js
@@ -0,0 +1,121 @@
+var connect = require('connect')
+  , http = require('http')
+  , assert = require('assert')
+  , tokenator = require('../lib/tokenator');
+
+function accessGranted(req, res){
+  res.end('Tokenator says yes!');
+}
+
+var app = connect()
+  .use(tokenator(['5917f138c80b512d14a4ee2fe05a17dc', '7b1a47ab847f7534b507c6ae4a763118']))
+  .use(accessGranted)
+
+
+describe('Making a get request', function(){
+  before(function(){
+    http.Server(app).listen(3000);
+  })
+  describe('with a valid token', function(){
+    it('should get return a 200 response', function(done){
+      http.get( { 
+        host: '127.0.0.1', 
+        port: '3000', 
+        headers: { 
+          'api-token': '7b1a47ab847f7534b507c6ae4a763118' 
+        } 
+      }, function(res){
+        assert.strictEqual(res.statusCode, 200);
+        done();
+      })
+    })
+    it('should return the correct body content', function(done){
+      http.get( { 
+        host: '127.0.0.1', 
+        port: '3000', 
+        headers: { 
+          'api-token': '7b1a47ab847f7534b507c6ae4a763118' 
+        } 
+      }, function(res){
+        var body = '';
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          assert.strictEqual(body, 'Tokenator says yes!');
+          done();
+        });
+      })
+    })
+  })
+  describe('with no token', function(){
+    it('should get return a 401 response', function(done){
+      http.get( { host: '127.0.0.1', port: '3000' }, function(res){
+        assert.strictEqual(res.statusCode, 401);
+        done();
+      })
+    })
+    it('should have an application/json header', function(done){
+      http.get( { host: '127.0.0.1', port: '3000' }, function(res){
+        assert.strictEqual(res.headers['content-type'], 'application/json');
+        done();
+      })
+    })
+    it('should return an error message as json', function(done){
+      http.get( { host: '127.0.0.1', port: '3000' }, function(res){
+        var body = '';
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          assert.strictEqual(body, '{"errors":"An API token is required."}');
+          done();
+        });
+      })
+    })
+  })
+  describe('with a invalid token', function(){
+    it('should get return a 401 response', function(done){
+      http.get( { 
+        host: '127.0.0.1', 
+        port: '3000', 
+        headers: { 
+          'api-token': 'invalidtoken' 
+        } 
+      }, function(res){
+        assert.strictEqual(res.statusCode, 401);
+        done();
+      })
+    })
+    it('should have an application/json header', function(done){
+      http.get( { 
+        host: '127.0.0.1', 
+        port: '3000', 
+        headers: { 
+          'api-token': 'invalidtoken' 
+        } 
+      }, function(res){
+        assert.strictEqual(res.headers['content-type'], 'application/json');
+        done();
+      })
+    })
+    it('should return an error message as json', function(done){
+      http.get( { 
+        host: '127.0.0.1', 
+        port: '3000', 
+        headers: { 
+          'api-token': 'invalidtoken' 
+        } 
+      }, function(res){
+        var body = '';
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          assert.strictEqual(body, '{"errors":"Your API token is invalid."}');
+          done();
+        });
+      })
+    })
+  })
+})
